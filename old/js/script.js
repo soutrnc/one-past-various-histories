9fc86f919a28d5874d8a86e7a8c92f1600be83b6
Sout <ritzvicenteculanag@outlook.com>
2022-11-17 12:41:03 +0800

    reset project

---
+++ old/js/script.js
@@ -0,0 +1,18 @@
+const cards = document.getElementsByClassName("card");
+
+// let intervals = 0;
+// let opacity = 1;
+// let zIndex = 8;
+// const setOpacityOfCards = () => {
+//   Array.from(cards).forEach((x) => {
+//     x.style.transform = `translate(${intervals.toString()}px)`;
+//     x.style.backgroundColor = `rgba(25,25,25,${opacity})`;
+//     x.style.zIndex = zIndex--;
+
+//     intervals += 85;
+//     opacity -= 0.125;
+//   });
+// };
+
+// // Method invocations
+// setOpacityOfCards();
